e5e1bc31eb3793900b44b33e5d09fe28ec0eee74
Guilherme Sabbado <gss.sabbado@gmail.com>
2025-07-20 19:00:22 -0300

    att req

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
-import json
 
 # Inicializa Firebase
 if not firebase_admin._apps:
-    cred_dict = json.loads(st.secrets["firebase"].to_json())
+    cred_dict = dict(st.secrets["firebase"])
     cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
 
