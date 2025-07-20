8dad29dfbb38eb04ae1a1f6b6f9cdde73076d169
Guilherme Sabbado <gss.sabbado@gmail.com>
2025-07-20 18:54:02 -0300

    att req

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from firebase_admin import credentials, firestore
 
 # Inicializa Firebase
 if not firebase_admin._apps:
-    cred = credentials.Certificate("key.json")
+    cred_dict = st.secrets["firebase"]
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
 
 # Conecta com o Firestore
